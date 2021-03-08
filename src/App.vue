cbe67a4fb08d77a96a63f6c325c175438813bbec
Dami Ajayi <ajayidamey@gmail.com>
2021-03-09 00:43:13 +0100

    temporary web design

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <landing-page />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './pages/LandingPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
 
+<style src="./assets/css/reset.css"></style>
+<style src="./assets/css/main.css"></style>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div class="landing-page">
+      <header>
+          <h1 class="landing-page__title">Wedx.Africa</h1>
+      </header>
+      <hr>
+      <div class="landing-page__description">
+          <p>Redefining 'Fun' Wedding Experiences</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'landing-page',   
+};
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  height: 100vh;
+  display: flex;
+  max-width: 1440px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border: 5px solid var(--white);
+}
+.landing-page {
+  max-width: 51rem;
+  padding: 4rem 1.2rem;
+}
+.landing-page__description {
+  font-size: 1.2rem;
+  margin: 1.2rem 0;
+  font-weight: 500;
+  line-height: 1.4;
+  animation: 4s appearLater;
+}
+hr {
+  width: 100%;
+  border: 0.5px solid rgba(255, 255, 255, 0.12);
+  margin: 3rem 0;
+}
+.landing-page__title {
+  animation: 2.5s appear;
+  margin: auto;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+@keyframes appearLater {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+}
+@media (min-width: 48rem) {
+  .container {
+    justify-content: center;
+    border: 10px solid var(--white);
+    padding: 4rem 0 0;
+  }
+  /* header {
+    text-align: center;
+  } */
+  .landing-page__description {
+    font-size: 1.8rem;
+    margin: 1.2rem;
+    line-height: 1.4;
+  }
+}
+</style>
